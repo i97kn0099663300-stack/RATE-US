38eadc7e490631459f7e29f722d44ca3ef0e9622
i97kn0099663300-stack <i97kn0099663300@gmail.com>
2026-06-08 19:29:48 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -104,4 +104,5 @@ def delete_rating(rating_id):
     return jsonify({'message': 'تم الحذف'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
